42741cd9d5f8157b8b9012b522ac710743ec5e40
Santosh <santoshkalwar@gmail.com>
2023-02-08 16:54:19 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const form = document.querySelector('form')!;
 
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-const GOOGLE_API_KEY = 'AIzaSyAuyPWb0vgdZ3j5PZ6ptox5pFoEUMoYMao';
+const GOOGLE_API_KEY = 'INSERTYOURGOOGLEAPIKEY';
 
 type GoogleGeoCodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
